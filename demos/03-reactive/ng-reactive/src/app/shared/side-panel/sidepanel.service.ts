8bcd19caf0bfcd78c40cdbc26d1240aac61e3bfc
Alexander Pajer <alexander.pajer@integrations.at>
2023-04-16 10:06:51 +0200

    cleanup

--- demos/03-reactive/ng-reactive/src/app/shared/side-panel/sidepanel.service.ts
+++ demos/03-reactive/ng-reactive/src/app/shared/side-panel/sidepanel.service.ts
@@ -1,13 +1,8 @@
 import { Injectable, computed, signal } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 import { SidebarActions } from './sidebar.actions';
-import { computeMsgId } from '@angular/compiler';
 
 @Injectable({ providedIn: 'root' })
 export class SidePanelService {
-  // RxJS Version using BehaviourSubject
-  // private commands: BehaviorSubject<SidebarActions> =
-  //   new BehaviorSubject<SidebarActions>(SidebarActions.HIDE_MARKDOWN);
   private commands = signal<SidebarActions>(SidebarActions.HIDE_MARKDOWN);
 
   getCommands() {
